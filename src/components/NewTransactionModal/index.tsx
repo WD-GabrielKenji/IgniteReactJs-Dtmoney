7973510bf7a5464f8fd4c6e244ffb84d8ee98198
WD-G.Kenji <g.kenjiJS@hotmail.com>
2021-05-22 20:59:26 -0300

    Style: Estrutura e Estilização do Formulario e do Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
 
 interface NewTransactionModal {
     isOpen: boolean;
@@ -8,10 +10,30 @@ interface NewTransactionModal {
 export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionModal){
     return (
         <Modal 
-                isOpen={isOpen}
-                onRequestClose={onRequestClose}
-            >
-                <h2>Cadastrar translação</h2> 
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            > 
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar translação</h2>
+
+                <input placeholder="Titulo" />
+                <input type="number" placeholder="Valor" />
+                <input placeholder="Categoria" />
+
+                <button type="submit">Cadastrar</button>
+
+            </Container>
         </Modal>
     )
 }
