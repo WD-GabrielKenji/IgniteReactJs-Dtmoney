1c80cf39ea2e5e3dc8f0be42898f16d184a9bfc4
WD-G.Kenji <g.kenjiJS@hotmail.com>
2021-05-23 13:20:15 -0300

    Feat: Inserindo dados na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
+import { api } from '../../services/api';
 
 interface NewTransactionModal {
     isOpen: boolean;
@@ -20,12 +22,14 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
     function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
 
-        console.log({
+        const data = {
             title,
             value,
             category,
             type,
-        })
+        }
+
+        api.post('/transactions', data)
     }
 
     return (
