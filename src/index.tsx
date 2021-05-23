328aeea8654e7fd628d07c876b2cae191805ded0
WD-G.Kenji <g.kenjiJS@hotmail.com>
2021-05-23 14:26:34 -0300

    Feat: Listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,35 @@ createServer ({
         transaction: Model,
     },
 
+    seeds(server) {
+        server.db.loadData({
+          transactions: [
+            {
+              id: 1,
+              title: 'Freelance de website',
+              type: 'deposit',
+              category: 'Dev',
+              amount: 6000,
+              createdAt: new Date('2021-03-15 08:00:00')
+            },
+    
+            {
+              id: 2,
+              title: 'Aluguel',
+              type: 'withdraw',
+              category: 'Casa',
+              amount: 1100,
+              createdAt: new Date('2021-03-14 11:00:00')
+            }
+          ]
+        });
+      },
+
     routes(){
         this.namespace = 'api';
     
         this.get('/transactions', () => {
-            return this.schema.all('transactions');
+            return this.schema.all('transaction');
         })
 
         this.post('/transactions', (schema, request) => {
